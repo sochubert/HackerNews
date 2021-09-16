1f3ece2328029137c52027d177020c1d389b712f
sochubert <sochubert@gmail.com>
2021-09-16 15:26:59 +0900

    Detail View

---
+++ H4XOR News/Views/DetailView.swift
@@ -0,0 +1,24 @@
+//
+//  DetailView.swift
+//  H4XOR News
+//
+//  Created by Minjae Lee on 2021/09/16.
+//
+
+import SwiftUI
+import WebKit
+
+struct DetailView: View {
+    
+    let url: String?
+    
+    var body: some View {
+        WebView(urlString: url)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(url: "https://www.google.com")
+    }
+}
---
+++ H4XOR News/Views/WebView.swift
@@ -0,0 +1,29 @@
+//
+//  WebView.swift
+//  H4XOR News
+//
+//  Created by Minjae Lee on 2021/09/16.
+//
+
+import Foundation
+import WebKit
+import SwiftUI
+
+struct WebView: UIViewRepresentable {
+    
+    let urlString: String?
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString = urlString {
+            if let url = URL(string: safeString) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+    
+}
